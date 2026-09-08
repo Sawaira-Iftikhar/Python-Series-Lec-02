2b3428cfd58deed51bb319a6776704368a74521a
Sawaira-Iftikhar <isawairaiftikhar@gmail.com>
2026-09-09 01:50:00 +0500

    add new file and setup

---
+++ 01_Strings_Basics.py
@@ -0,0 +1,7 @@
+"""
+============================================
+  LECTURE 2 - FILE 1: STRINGS BASICS
+  Topics: Strings, Escape Sequences, Indexing
+  Total Questions: 
+============================================
+"""
