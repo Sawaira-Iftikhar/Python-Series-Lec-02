85e93f39fa61d09c7c5571f33c9fd8d58f3f0c5a
Sawaira-Iftikhar <isawairaiftikhar@gmail.com>
2026-09-09 01:58:35 +0500

    write the code of Q1

--- 01_Strings_Basics.py
+++ 01_Strings_Basics.py
@@ -15,3 +15,13 @@
 #     b) Double quotes
 #     c) Triple quotes (multi-line)
 #     Print all three.
+
+string1 = "My name is Sawaira."
+string2 = 'I am university Student.'
+string3 = """what!
+           do you mean i got the
+           chance """
+
+print("Double quotes: ", string1)
+print("Single quotes: ", string2)
+print("Triple quotes: ", string3)
