a8558a3f59e041e0f8092a3acb8d3e45b476fa9e
Sawaira-Iftikhar <isawairaiftikhar@gmail.com>
2026-09-10 06:08:11 +0500

    add comment for better understanding

--- 02_Slicing_and_Method.py
+++ 02_Slicing_and_Method.py
@@ -22,7 +22,11 @@
 name = "Ahtsham"
 # first 3 characters
 print("first 3: ",name[0:3])
+#Character form index 2 to 6
 print("2 to 6: ", name[2:7])
+# last 4 characters using positive indices
 print("last 4: ",name[6:7])
+# Everything from index 5 to te end
 print("5 to end: ",name[5:len(name)])
+# Te entire string using slicing
 print("full: ",name[0:7])
