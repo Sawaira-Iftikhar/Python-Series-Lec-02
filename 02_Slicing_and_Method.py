e0d8a9a1f8f63624fda570a728de39b6d5655192
Sawaira-Iftikhar <isawairaiftikhar@gmail.com>
2026-09-10 05:47:18 +0500

    details of second file

---
+++ 02_Slicing_and_Method.py
@@ -0,0 +1,8 @@
+"""
+============================================
+  LECTURE 2 - FILE 2: SLICING & METHODS
+  Topics: Slicing, Negative Slicing, String Methods
+  Total Questions: 
+============================================
+
+"""
