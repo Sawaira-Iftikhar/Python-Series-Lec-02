ebe6db5f4f659119fbb7c3479fc70af76f224601
Sawaira-Iftikhar <isawairaiftikhar@gmail.com>
2026-09-10 05:50:46 +0500

    write Q1 in slicing

--- 02_Slicing_and_Method.py
+++ 02_Slicing_and_Method.py
@@ -10,3 +10,11 @@
 # ==========================================
 #  PART A: SLICING 
 # ==========================================
+
+# Q1. Given: text = "ABCDEFGHIJ"
+#     Extract and print the following using slicing [start:stop]:
+#     a) First 3 characters
+#     b) Characters from index 2 to 6
+#     c) Last 4 characters (using positive indices)
+#     d) Everything from index 5 to the end
+#     e) The entire string using slicing
