636f8606ad5f3bb4678d9a73f9bf0a75e011b6f1
Sawaira-Iftikhar <isawairaiftikhar@gmail.com>
2026-09-12 22:20:49 +0500

    add details for file 3

---
+++ 03_conditionals.py
@@ -0,0 +1,8 @@
+"""
+============================================
+  LECTURE 2 - FILE 3: CONDITIONAL STATEMENTS
+  Topics: if, elif, else, Nested, Ternary
+  Total Questions: 
+============================================
+
+"""
