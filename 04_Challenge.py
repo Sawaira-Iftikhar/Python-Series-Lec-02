adacb8fb18ce8e773839b3e374c3b45bb26eb01c
Sawaira-Iftikhar <isawairaiftikhar@gmail.com>
2026-09-13 19:33:55 +0500

    add Details of 4th file

---
+++ 04_Challenge.py
@@ -0,0 +1,10 @@
+"""
+============================================
+  LECTURE 2 - FILE 4: BOSS CHALLENGE 
+  Topics: ALL 6 Topics Combined
+  Total Challenges: 
+============================================
+  These problems combine Strings, Slicing,
+  Methods, and Conditionals together.
+============================================
+"""
